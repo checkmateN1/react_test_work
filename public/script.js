6da112ee6c156344290c8896dc71b53ecb1fd7fd
Evgeniy Pushkar <checkmaten1@gmail.com>
2019-03-04 12:24:44 +0200

    work on section2

--- public/script.js
+++ public/script.js
@@ -15,16 +15,16 @@ window.onresize = () => {
 let resize = () => {
     let innerWidth = window.innerWidth;
     let clientWidth = document.documentElement.clientWidth;
-
+    let bgr = document.querySelector('.test-assignment-intro .background-wrapper');
     //console.log(innerWidth);  // with scroll   1041
     //console.log(clientWidth);  // without scroll  1024
 
     let diff = innerWidth - clientWidth;
     if(diff > 0 && clientWidth + diff > 1024 && clientWidth < 1024) {
-        document.querySelector('.test-assignment-intro .background-wrapper').style.backgroundPositionY = 0;
-        document.querySelector('.test-assignment-intro .background-wrapper').style.backgroundSize  = `${1326 - (1024 - clientWidth)/0.6}px`;
+        bgr.style.backgroundPositionY = 0;
+        bgr.style.backgroundSize  = `${1326 - (1024 - clientWidth)/0.6}px`;
     } else {
-        document.querySelector('.test-assignment-intro .background-wrapper').removeAttribute('style');
+        bgr.removeAttribute('style');
         document.querySelector('.test-assignment-intro').removeAttribute('style');
     }
 };
